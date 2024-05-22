531f6ca2153a8e848278f27b9a4614fb4659e58c
OtavioAGO <otavioago@outlook.com>
2024-05-22 18:24:59 -0300

    Checar se bateu adicionada

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define GRN "\e[0;32m"
+#define YEL "\e[0;33m"
+#define SC "\e[0m"
+#define altura 16
+#define largura 32
+#define qtdCanos 3
+ typedef struct{
+    int x,y;
+ }pos;
+pos passaro;
+pos canos[3];
 
+int bateu(){
+    if(passaro.y == 15){
+        return 0;
+    }
+    for(int i = 0; i < qtdCanos; i++){
+        if ((passaro.x < x+2 && passaro.x > x-2) && (passaro.y ))
+    }
+}
 int main()
 {
-    printf("Hello world!\n");
+
     return 0;
 }
